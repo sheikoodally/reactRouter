1d3cf78f76aef1dd9aef84b67fe8604657154c4f
sheik oodally <sheikoodally@hotmail.com>
2019-10-01 20:11:11 -0400

    adding simple nav design css

--- reactRouter/src/App.js
+++ reactRouter/src/App.js
@@ -9,11 +9,21 @@ import Page3 from "./components/Page3.js";
 function App() {
   return (
     <div className="App">
+      <nav>
         <h1>hello</h1>
-        <Home/>
-        <Page1/>
-        <Page2/>
-        <Page3/>
+
+        <ul>
+            <li><a> <Home/> </a></li>
+            <li><a> <Page1/> </a></li>
+            <li><a> <Page2/> </a></li>
+            <li><a> <Page3/> </a></li>
+        </ul>
+
+          <Home/>
+          <Page1/>
+          <Page2/>
+          <Page3/>
+        </nav>
     </div>
   );
 }
