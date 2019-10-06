f9507513be575f8db438d01e78da3a5db46df075
sheikoodally <44279883+sheikoodally@users.noreply.github.com>
2019-10-05 21:33:08 -0400

    adding details

--- reactRouter/src/App.js
+++ reactRouter/src/App.js
@@ -5,24 +5,23 @@ import Home from "./components/Home.js";
 import Page1 from "./components/Page1.js";
 import Page2 from "./components/Page2.js";
 import Page3 from "./components/Page3.js";
+import Nav from "./components/Nav.js";
+
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <nav>
-        <h1>hello</h1>
-
-        <ul>
-            <li><a> <Home/> </a></li>
-            <li><a> <Page1/> </a></li>
-            <li><a> <Page2/> </a></li>
-            <li><a> <Page3/> </a></li>
-        </ul>
-
-          <Home/>
-          <Page1/>
-          <Page2/>
-          <Page3/>
+      <Router>
+          <Nav/>
+          <Switch>
+            <Route path="/" exact component={Home}/>
+            <Route path="/page1" component={Page1}/>
+            <Route path="/page2" component={Page2}/>
+            <Route path="/page3" component={Page3}/>
+          </Switch>
+        </Router>
         </nav>
     </div>
   );
